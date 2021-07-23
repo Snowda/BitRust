077040906217379eae4bd99d5cb54285161d1d66
Snowda <conor@dusklab.com>
2021-07-22 22:46:39 -0400

    Create program.rs

---
+++ program.rs
@@ -0,0 +1,16 @@
+
+
+fn main() {
+    println!("start here");
+    // file writing
+    // numpy equivalent
+    // database interface
+    // apache arrow interface
+    // unix value generator
+    // async network requests
+    // HTTP3
+    // email sending
+    // datetime management
+    // kafka
+    // pager duty
+}
