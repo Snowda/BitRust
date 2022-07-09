1521fa50502cd05f838d5860ded0d859beab0024
Snowda <conor@dusklab.com>
2022-07-09 17:24:21 -0400

    Performance test compiling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 use std::time::{Duration, Instant};
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref LOOKUP4: [char; 128] = {
+        let mut table = ['a'; 128];
+
+        for i in 0..128 {
+            table[i] = get_lookup_string((i as u8) as char) as char;
+        }
+
+        table
+    };
+}
 
 fn main() {
     // We can use this function here, and define it somewhere later
@@ -21,18 +34,22 @@ fn main() {
 
 
     // Initialize lookup table.
-    let mut table: [String; 128] = [0; 128];
+    let mut table: [char; 128] = ['a'; 128];
     for i in 0..128 {
-        table[i] = get_lookup_string(i.to_string()) as u8;
+        
+        table[i] = get_lookup_string((i as u8) as char) as char;
     }
     
     // Version 1: use match.
     let now1 = Instant::now();
     let mut result1: u64 = 0;
     for _ in 0..10000000 {
-        for c in "abcdef".chars() {
-            result1 += get_lookup_string(c.to_string()) as u64;
-        }
+        result1 += get_lookup_string('a') as u64;
+        result1 += get_lookup_string('b') as u64;
+        result1 += get_lookup_string('c') as u64;
+        result1 += get_lookup_string('d') as u64;
+        result1 += get_lookup_string('e') as u64;
+        result1 += get_lookup_string('f') as u64;
     }
     println!("RESULT: {}", result1);
     println!("  TIME: {} ms", now1.elapsed().as_millis());
@@ -41,45 +58,54 @@ fn main() {
     let now2 = Instant::now();
     let mut result2: u64 = 0;
     for _ in 0..10000000 {
-        for c in "abcdef".chars() {
-            result2 += table[c.to_string()] as u64;
-        }
+        result2 += table['a' as usize] as u64;
+        result2 += table['b' as usize] as u64;
+        result2 += table['c' as usize] as u64;
+        result2 += table['d' as usize] as u64;
+        result2 += table['e' as usize] as u64;
+        result2 += table['f' as usize] as u64;
     }
     println!("RESULT: {}", result2);
     println!("  TIME: {} ms", now2.elapsed().as_millis());
 
     // Verions 3: lazy static map
-
+    
     let now3 = Instant::now();
     let mut result3: u64 = 0;
 
-    // TODO implementation
     for _ in 0..10000000 {
-        for c in "abcdef".chars() {
-            // TODO result3 += table[c.to_string()] as u64;
-        }
+        result3 += LOOKUP4['a' as usize] as u64;
+        result3 += LOOKUP4['b' as usize] as u64;
+        result3 += LOOKUP4['c' as usize] as u64;
+        result3 += LOOKUP4['d' as usize] as u64;
+        result3 += LOOKUP4['e' as usize] as u64;
+        result3 += LOOKUP4['f' as usize] as u64;
     }
 
     println!("RESULT: {}", result3);
     println!("  TIME: {} ms", now3.elapsed().as_millis());
 }
 
-fn get_lookup_value(x: u8) -> u8 {
+fn lookup(c: char) -> u64{
+    return LOOKUP4[c as usize] as u64;
+}
+
+fn get_lookup_value(x: char) -> u8 {
     return match x {
-        b'a' => 30,
-        b'b' => 70,
-        b'c' => 10,
-        b'd' => 10,
-        b'e' => 35,
+        'a' => 30,
+        'b' => 70,
+        'c' => 10,
+        'd' => 10,
+        'e' => 35,
         _ => 0
     };
 }
 
-fn get_lookup_string(x: String) -> u8 {
+fn get_lookup_string(x: char) -> u8 {
     return match x {
-        "a" => 1,
-        "b" => 2,
-        "c" => 3,
+        'a' => 1,
+        'b' => 2,
+        'c' => 3,
         _ => 4
     };
 }
